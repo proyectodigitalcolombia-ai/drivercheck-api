7d729113d03b05d8bdc904080c8d3ee807820ba7
proyectodigitalcolombia-ai <proyectodigitalcolombia@gmail.com>
2026-03-07 21:57:33 -0500

    Add API key middleware to checkDriver route

--- src/routes/driverRoutes.js
+++ src/routes/driverRoutes.js
@@ -1,9 +1,9 @@
 const express = require("express")
-
 const router = express.Router()
 
 const driverController = require("../controllers/driverController")
+const apiKeyMiddleware = require("../middleware/apiKey")
 
-router.post("/check", driverController.checkDriver)
+router.post("/check", apiKeyMiddleware, driverController.checkDriver)
 
 module.exports = router
